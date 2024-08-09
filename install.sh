b53046f1780e332adfbd2f6ea1bdd3ca2978253f
Lior Levy <lioruby@macbook-pro.home>
2024-08-09 22:56:21 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,28 @@
+backup() {
+  target=$1
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.backup"
+      echo "-----> Moved your old $target config file to $target.backup"
+    fi
+  fi
+}
+
+#!/bin/zsh
+for name in *; do
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ] && [[ ! "$name" =~ '\.sublime-settings$' ]]; then
+      backup $target
+
+      if [ ! -e "$target" ]; then
+        echo "-----> Symlinking your new $target"
+        ln -s "$PWD/$name" "$target"
+      fi
+    fi
+  fi
+done
+
+zsh ~/.zshrc
+
+echo "Setup finished ! 👌"
